9559c43f7f4adaafffc287b74bb974ce7562e185
frostyausty <priceaustin62@gmail.com>
2020-10-11 09:09:54 -0500

    established variables with javascript

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,28 @@
+var key = "2ad6786df1447b809884ecabeea4490a";
+var city="Madison";
+
+fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=Madison&units=imperial&appid=2ad6786df1447b809884ecabeea4490a'
+    )
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                // var userInput = document.querySelector("#");
+                // console.log(userInput);
+
+                var currentTemp = data.main.temp;
+                console.log(currentTemp);
+
+                var humidity = data.main.humidity;
+                console.log(humidity + "%");
+
+                var windSpeed = data.wind.speed;
+                console.log(windSpeed);
+
+               // var uvIndex = ;
 
 
+            });
+        };
+
+    })
