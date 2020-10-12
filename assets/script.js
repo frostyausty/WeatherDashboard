583aa72e1044d8ad4b067bee86a1e0302edb3c55
frostyausty <priceaustin62@gmail.com>
2020-10-11 23:45:02 -0500

    added some data to the page, ran out of time to finish

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,13 @@ var cityFormEl = document.querySelector("#searchBar");
 var cityInputEl = document.querySelector("#city");
 var searchButtonEl = document.querySelector("#button");
 
+var cityNameEl = document.querySelector("#searchedCity");
+var weatherDateEl = document.querySelector("#searchedDate");
+var weatherIconEl = document.querySelector("#searchedIcon");
+var currentTempEl = document.querySelector("#p1");
+var currentHumidtyEl = document.querySelector("#p2");
+var currentWindEl = document.querySelector("#p3");
+
 
 var getCityWeather = function(city) {
     var apiUrl="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
@@ -12,21 +19,26 @@ fetch(apiUrl)
             response.json().then(function(data) {
 
                var cityName = data.name;
+               cityNameEl.textContent= cityName;
                console.log(cityName);
 
                var currentDate = data.dt;
+               weatherDateEl.textContent=currentDate;
                console.log(currentDate);
 
-               var weatherIcon = data.weather.icon;
-               console.log(weatherIcon);
+            //    var weatherIcon = data.weather.icon;
+            //    weatherIconEl.appendChild(weatherIcon);
 
                var currentTemp = data.main.temp;
+               currentTempEl.textContent=currentTemp;
                console.log(currentTemp);
 
                var humidity = data.main.humidity;
-               console.log(humidity + "%");
+               currentHumidtyEl.textContent=humidity;
+               console.log(humidity);
 
                var windSpeed = data.wind.speed;
+               currentWindEl.textContent=windSpeed;
                console.log(windSpeed);
 
             });
